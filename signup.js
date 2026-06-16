003d085491d474990179fdc2fb71d32a98f6e3e6
imaninaitore <imaninaitore1726@gmail.com>
2026-06-16 12:06:45 +0300

    remove profile section in member details

---
+++ signup.js
@@ -0,0 +1,17 @@
+const signupForm = document.getElementById('signup-form');
+const username  = document.getElementById('name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+signupForm.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    const usernameValue = username.value;
+    const emailValue = email.value;
+    const passwordValue = password.value;
+
+
+});
+
+
+
