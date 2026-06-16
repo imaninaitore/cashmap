3524cd20180cbd9af21de8ade7d910276afe7097
imaninaitore <imaninaitore1726@gmail.com>
2026-06-16 12:49:40 +0300

    replace manual object creation with User class as the blueprint

--- signup.js
+++ signup.js
@@ -3,6 +3,14 @@ const username  = document.getElementById('name');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 
+class User{
+    constructor(username,email,password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
+
 let users = JSON.parse(localStorage.getItem("users")) || [];
 
 signupForm.addEventListener("submit", function(event){
@@ -12,10 +20,10 @@ signupForm.addEventListener("submit", function(event){
     const emailValue = email.value;
     const passwordValue = password.value;
 
-    const user = {
-        username: usernameValue,
-        email: emailValue,
-        password: passwordValue
+    const user = new User{
+     usernameValue,
+     emailValue,
+     passwordValue
     };
 
     users.push(user)
