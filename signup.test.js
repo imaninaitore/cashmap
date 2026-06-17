389311d75e147ac9b8e789d2e12d71985e51a521
imaninaitore <imaninaitore1726@gmail.com>
2026-06-17 09:36:18 +0300

    change test expect to match the return as an object not a string

--- signup.test.js
+++ signup.test.js
@@ -1,8 +1,15 @@
 const{signUpUser} = require("./signup") //importing function
 describe("Test for Signup", () =>{
     it("should successfully create an account",() =>{
-     expect(signUpUser("username","email","password"))
-    .toBe("account creation successful");   
+
+        const user = signUpUser("Imani","imani@gmail.com","1234");
+
+     expect(user.username)
+     .toBe("Imani");
+     expect(user.email)
+     .toBe("imani@gmail.com");
+     expect(user.password)
+     .toBe("1234");
     });
     
 });
