b41078016f7667ffee74bfc43f8d7b321dd737cf
imaninaitore <imaninaitore1726@gmail.com>
2026-06-17 09:48:47 +0300

    seclude place code to test and import to test file

---
+++ signup2.js
@@ -0,0 +1,11 @@
+class User{
+    constructor(username,email,password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
+
+function signUpUser(username,email,password){
+    return new User(username,email,password);
+}
