26ca434f6997c35654690109429296c951d8206c
imaninaitore <imaninaitore1726@gmail.com>
2026-06-17 12:56:07 +0300

    correct syntax errors of variable names and place function code inside the event listener

--- login.js
+++ login.js
@@ -1,7 +1,16 @@
+const loginForm = document.getElementById("login-form");
+const usernameInput = document.getElementById("username")
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+
+//submit event
+loginForm.addEventListener("submit", function(event){
+ event.preventDefault();
+
 //get login inputs
-const username = username.value
-const email = email.value
-const password = password.value
+const usernameValue = usernameInput.value
+const emailValue = emailInput.value
+const passwordValue = passwordInput.value
 
 //get all users from local storage
 let users =JSON.parse(localStorage.getItem ("users")) || [];
@@ -10,20 +19,19 @@ let users =JSON.parse(localStorage.getItem ("users")) || [];
 const correctUser = users.find(user =>{
 return user.email === emailValue && user.password === passwordValue;
 
+});
+
 //condition for no match found
-if(!loggedInUser) {
+if(!correctUser) {
     alert("wrong log in details or not signed in yet")
     return;
 }
-
 //save the logged in user
-localStorage.setItem("currentUser", JSON.stringify(loggedInUser));
+localStorage.setItem("currentUser", JSON.stringify(correctUser));
+//redirect
 window.location.href = "member.html"
 
-})
-
-
-
+});
 
 
 
