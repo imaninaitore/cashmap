7c2f50f024e1306907ee739e33a5f6d7e2ad508d
imaninaitore <imaninaitore1726@gmail.com>
2026-06-18 07:46:48 +0300

    add if function to secluded file 2 for testing

--- login2.js
+++ login2.js
@@ -4,5 +4,10 @@ function logInUser(usernameValue,emailValue,passwordValue){
     const correctUser = users.find(user => {
         return user.email === emailValue && user.password === passwordValue;
     });
-    
+
+    if(!correctUser) return{success: false, message:"wrong log in details or not signed in yet"};
+
+    localStorage.setItem("currentUser", JSON.stringify(correctUser));
+
+  return { success: true, user: correctUser };
 }
