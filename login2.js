b615bbe855d63d55e03b06d344c091b3cd85d3f1
imaninaitore <imaninaitore1726@gmail.com>
2026-06-19 12:17:38 +0300

    add detailed comments on line 2 and 10

--- login2.js
+++ login2.js
@@ -1,5 +1,5 @@
-function logInUser(usernameValue,emailValue,passwordValue){
-    let users = JSON.parse(localStorage.getItem("users")) || [] ;
+function logInUser(emailValue,passwordValue){
+    let users = JSON.parse(localStorage.getItem("users")) || [] ; //get users form local storage, convert string to array, if nothing exists create empty array
 
     const correctUser = users.find(user => {
         return user.email === emailValue && user.password === passwordValue;
@@ -7,7 +7,7 @@ function logInUser(usernameValue,emailValue,passwordValue){
 
     if(!correctUser) return{success: false, message:"wrong log in details or not signed in yet"};
 
-    localStorage.setItem("currentUser", JSON.stringify(correctUser));
+    localStorage.setItem("currentUser", JSON.stringify(correctUser)); //get from storage and convert to javascript object
 
   return { success: true, user: correctUser };
 }
