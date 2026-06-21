996b5246c4c0dd0fca12bb648aa621aa013f597f
imaninaitore <imaninaitore1726@gmail.com>
2026-06-21 20:54:25 +0300

    member specific login with display of name,email and phonenumber

---
+++ member.js
@@ -0,0 +1,16 @@
+//retrieve logged-in user
+const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+//check if user exists
+if(!currentUser){
+     window.location.href = "login.html";
+}
+//display their info
+document.getElementById("welcome").textContent =
+`Welcome back ${currentUser.username} !`;
+
+document.getElementById("member-email").textContent =
+`Email: ${currentUser.email}`;
+
+document.getElementById("member-phone").textContent =
+`Phone: ${currentUser.phoneNumber}`;
+
