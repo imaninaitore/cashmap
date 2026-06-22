1d98a9f9f9dcf23e742fae1939320adda4a63a58
imaninaitore <imaninaitore1726@gmail.com>
2026-06-22 05:08:28 +0300

    write function for calculating spent in budget and the remaining

--- member.js
+++ member.js
@@ -44,3 +44,38 @@ document.getElementById("savings-account").textContent =
 accountUser["savings account balance"];
 
 });
+
+//monthly budget section
+//get elements
+const totalBudget = document.getElementById("total-budget");
+const usedBudget = document.getElementById("used-money");
+const remainingBudget = document.getElementById("remaining-money");
+
+//display to show
+let totalBudget = 0;
+let expenses = [
+    { name: "Food", amount:0 },
+    { name: "Transport", amount: 0},
+    { name: "Shopping", amount: 0}
+    { name: "Bills", amount: 0}
+
+]
+
+// Function to update budget 
+function updateBudget() {
+
+    // Calculate total spent
+    const usedSoFar = expenses.reduce((sum, expense) => {
+        return sum + expense.amount;
+    }, 0);
+
+    // Calculate remaining
+    const remaining = totalBudget - usedSoFar;
+
+    // Display values
+    totalBudget.textContent = totalBudget;
+    usedBudget.textContent = usedSoFar;
+    remainingBudget.textContent = remaining;
+}
+
+updateBudget(); //call the function
